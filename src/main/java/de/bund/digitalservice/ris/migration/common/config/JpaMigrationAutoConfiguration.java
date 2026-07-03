f495ec1fc55383499d913e1680e022176bcb87d3
lucassolcher <l.solcher@gmail.com>
2026-07-03 11:52:44 +0200

    Add MigrationStatusService

--- src/main/java/de/bund/digitalservice/ris/migration/common/config/JpaMigrationAutoConfiguration.java
+++ src/main/java/de/bund/digitalservice/ris/migration/common/config/JpaMigrationAutoConfiguration.java
@@ -1,8 +1,12 @@
 package de.bund.digitalservice.ris.migration.common.config;
 
+import de.bund.digitalservice.ris.migration.common.repository.IncrementalMigrationStatusRepository;
 import de.bund.digitalservice.ris.migration.common.repository.jpa.JpaIncrementalMigrationStatusRepository;
+import de.bund.digitalservice.ris.migration.common.service.MigrationStatusService;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 
@@ -13,4 +17,12 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 @AutoConfiguration(after = MigrationAutoConfiguration.class)
 @ConditionalOnClass(JpaTransactionManager.class)
 @EnableJpaRepositories(basePackageClasses = JpaIncrementalMigrationStatusRepository.class)
-public class JpaMigrationAutoConfiguration {}
+public class JpaMigrationAutoConfiguration {
+
+  @Bean
+  @ConditionalOnMissingBean
+  public MigrationStatusService migrationStatusService(
+      IncrementalMigrationStatusRepository statusRepository) {
+    return new MigrationStatusService(statusRepository);
+  }
+}
