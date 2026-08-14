01976d95dd7634119c1178accbcb128103150964
Xavier Tremel <xavier.tremel@digitalservice.bund.de>
2026-08-14 14:35:46 +0200

    Bump spotless lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   `java-library`
   id("io.spring.dependency-management") version "1.1.7"
-  id("com.diffplug.spotless") version "8.8.0"
+  id("com.diffplug.spotless") version "8.9.0"
   id("jacoco")
   `maven-publish`
 }
