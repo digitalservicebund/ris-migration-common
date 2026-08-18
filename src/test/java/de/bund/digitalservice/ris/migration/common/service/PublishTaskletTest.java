6a16cdedb501943a5174909d92c4a7f78b93e3fe
andreas-deazevedo <andreas.deazevedo@digitalservice.bund.de>
2026-08-18 09:39:13 +0200

    RISDEV-12369 Add migration type reader

--- src/test/java/de/bund/digitalservice/ris/migration/common/service/PublishTaskletTest.java
+++ src/test/java/de/bund/digitalservice/ris/migration/common/service/PublishTaskletTest.java
@@ -21,15 +21,19 @@ class PublishTaskletTest {
   void execute_dailyMigration_uploadsAndSetsDailyExitStatus() throws Exception {
     var s3MigrationService = mock(S3MigrationService.class);
     var tasklet = new PublishTasklet(s3MigrationService, "/output");
-    StepExecution stepExecution = MetaDataInstanceFactory.createStepExecution(new JobParametersBuilder()
-            .addString("migrationType", MigrationType.DAILY.name()).toJobParameters());
+    StepExecution stepExecution =
+        MetaDataInstanceFactory.createStepExecution(
+            new JobParametersBuilder()
+                .addString("migrationType", MigrationType.DAILY.name())
+                .toJobParameters());
     StepContribution stepContribution = new StepContribution(stepExecution);
     ChunkContext chunkContext = new ChunkContext(new StepContext(stepExecution));
 
     RepeatStatus result = tasklet.execute(stepContribution, chunkContext);
 
     verify(s3MigrationService).uploadFolder("/output", MigrationType.DAILY);
-    assertThat(stepContribution.getExitStatus()).isEqualTo(new ExitStatus(PublishTasklet.DAILY_MIGRATION_COMPLETED));
+    assertThat(stepContribution.getExitStatus())
+        .isEqualTo(new ExitStatus(PublishTasklet.DAILY_MIGRATION_COMPLETED));
     assertThat(result).isEqualTo(RepeatStatus.FINISHED);
   }
 
@@ -37,28 +41,36 @@ class PublishTaskletTest {
   void execute_monthlyMigration_uploadsAndSetsMonthlyExitStatus() throws Exception {
     var s3MigrationService = mock(S3MigrationService.class);
     var tasklet = new PublishTasklet(s3MigrationService, "/output");
-    StepExecution stepExecution = MetaDataInstanceFactory.createStepExecution(new JobParametersBuilder()
-            .addString("migrationType", MigrationType.MONTHLY.name()).toJobParameters());
+    StepExecution stepExecution =
+        MetaDataInstanceFactory.createStepExecution(
+            new JobParametersBuilder()
+                .addString("migrationType", MigrationType.MONTHLY.name())
+                .toJobParameters());
     StepContribution stepContribution = new StepContribution(stepExecution);
     ChunkContext chunkContext = new ChunkContext(new StepContext(stepExecution));
 
     tasklet.execute(stepContribution, chunkContext);
 
     verify(s3MigrationService).uploadFolder("/output", MigrationType.MONTHLY);
-    assertThat(stepContribution.getExitStatus()).isEqualTo(new ExitStatus(PublishTasklet.MONTHLY_MIGRATION_COMPLETED));
+    assertThat(stepContribution.getExitStatus())
+        .isEqualTo(new ExitStatus(PublishTasklet.MONTHLY_MIGRATION_COMPLETED));
   }
 
   @Test
   void execute_nullS3Service_skipsUploadButStillSetsExitStatus() throws Exception {
     var tasklet = new PublishTasklet(null, "/output");
-    StepExecution stepExecution = MetaDataInstanceFactory.createStepExecution(new JobParametersBuilder()
-            .addString("migrationType", MigrationType.DAILY.name()).toJobParameters());
+    StepExecution stepExecution =
+        MetaDataInstanceFactory.createStepExecution(
+            new JobParametersBuilder()
+                .addString("migrationType", MigrationType.DAILY.name())
+                .toJobParameters());
     StepContribution stepContribution = new StepContribution(stepExecution);
     ChunkContext chunkContext = new ChunkContext(new StepContext(stepExecution));
 
     RepeatStatus result = tasklet.execute(stepContribution, chunkContext);
 
-    assertThat(stepContribution.getExitStatus()).isEqualTo(new ExitStatus(PublishTasklet.DAILY_MIGRATION_COMPLETED));
+    assertThat(stepContribution.getExitStatus())
+        .isEqualTo(new ExitStatus(PublishTasklet.DAILY_MIGRATION_COMPLETED));
     assertThat(result).isEqualTo(RepeatStatus.FINISHED);
   }
 }
