12e4628de6df96c69006dad815e2c59de7d66258
andreas-deazevedo <andreas.deazevedo@digitalservice.bund.de>
2026-08-18 09:47:22 +0200

    Set default version to 0.7.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 group = "de.bund.digitalservice.ris"
-version = System.getenv("RELEASE_VERSION") ?: "0.7.0"
+version = System.getenv("RELEASE_VERSION") ?: "0.7.3"
 
 java {
   toolchain {
